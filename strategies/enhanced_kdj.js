66e69f71573e77d9d4741fe7d00fd8cd464cb069
Jiaming Li <ljm625@gmail.com>
2017-07-06 22:52:48 -0700

    implement more advanced technique

--- strategies/enhanced_kdj.js
+++ strategies/enhanced_kdj.js
@@ -14,7 +14,8 @@ method.init = function() {
         over_buy:0,
         over_sell:0,
         previous_K:0,
-        previous_D:0
+        previous_D:0,
+        last_buy_price:0
     };
 
 
@@ -33,6 +34,13 @@ method.init = function() {
 
 method.update = function (candle) {
     // this.talibIndicators.kdj.run();
+    if (this.historyCandle.length < this.requiredHistory) {
+        this.historyCandle.unshift(candle);
+    }
+    else {
+        this.historyCandle.unshift(candle);
+        this.historyCandle.pop();
+    }
 }
 
 // For debugging purposes.
@@ -47,38 +55,79 @@ method.check = function(candle) {
     // use indicator results
     if(candle.volume===0) return; // Make sure we don't count when the website is down.
     var result = this.talibIndicators.kdj.result;
+
     // log.debug(result);
-    if(this.trend.action==='short'){
-        if(result.outSlowD>=settings.D_low_level || result.outSlowK>=settings.K_low_level){
-            if(this.trend.over_buy>=settings.period){
-                this.trend.action='long';
-                this.advice('long');
-            }
-            this.trend.over_buy=0;
+    if (this.trend.action === 'long') {
+        if ((candle.open + candle.close) / 2 < this.trend.last_buy_price * (1 - settings.force_short_threshold)) {
+            log.debug("Force selling!");
+            this.trend.action = 'short';
+            this.advice('short');
+            this.trend.last_hammer_max = 0;
             return;
         }
+    }
+    var avg_volume = 0;
+    for (var i = 1; i < settings.volume_avg_date + 1; i++) {
+        avg_volume += this.historyCandle[i].volume;
+    }
+    avg_volume = avg_volume / settings.volume_avg_date;
+
+
+    if(this.trend.action==='short'){
+        //检测金叉
+        //check the volume
+
+
+        if(result.outSlowD>=settings.D_low_level || result.outSlowK>=settings.K_low_level){
+            this.trend.over_buy=0;
+        }
         
-        if(result.outSlowD<settings.D_low_level && result.outSlowK<settings.K_low_level){
+        else if(result.outSlowD<settings.D_low_level && result.outSlowK<settings.K_low_level){
             log.debug("People started to overbuy");
             this.trend.over_buy+=1;
+
+            if (candle.volume >= avg_volume * settings.volume_multiply) {
+
+                if (this.trend.previous_D > this.trend.previous_K && result.outSlowD <= result.outSlowK) {
+                    //出现金叉
+                    if(this.trend.over_buy>=settings.period){
+                        this.trend.action='long';
+                        this.advice('long');
+                        this.trend.last_buy_price=(candle.open + candle.close)/2;
+                    }
+
+                }
+            }
             // this.trend.action='long';
             // this.advice('long');
         }
 
     }
     else if (this.trend.action==='long'){
+
+
         if(result.outSlowD<=settings.D_high_level || result.outSlowK<=settings.K_high_level){
-            if(this.trend.over_sell>=settings.period){
-                this.trend.action='short';
-                this.advice('short');
-            }
+
             this.trend.over_sell=0;
-            return;
         }
-        if(result.outSlowD>settings.D_high_level && result.outSlowK>settings.K_high_level){
+        else if(result.outSlowD>settings.D_high_level && result.outSlowK>settings.K_high_level){
             log.debug("Best Sell Point");
             this.trend.over_sell+=1;
+
+            if (candle.volume >= avg_volume * settings.volume_multiply) {
+
+                if (this.trend.previous_K > this.trend.previous_D && result.outSlowD >= result.outSlowK) {
+                    //出现金叉
+                    if(this.trend.over_sell>=settings.period){
+                        this.trend.action='short';
+                        this.advice('short');
+                    }
+                }
+            }
         }
     }
+    this.trend.previous_D=result.outSlowD;
+    this.trend.previous_K=result.outSlowK;
+
 };
 module.exports = method;
